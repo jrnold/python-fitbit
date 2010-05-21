924827d5cacfb86055f64ec0f92fdd606c7b6bcc
Jeffrey.Arnold <jeffrey.arnold@gmail.com>
2010-05-20 22:37:55 -0400

    completed dump2csv.py

--- examples/dump2csv.py
+++ examples/dump2csv.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python2.6
 """
 Script to dump fitbit data for a single day into
-csv files. 
+csv files.  
 
-Usage is
+Usage
 
-   python dump2sqlite.py 2010-05-14
+   python dump2csv.py 2010-05-14
+
 
 """
 from fitbit import Client
@@ -16,8 +17,22 @@ import ConfigParser
 import datetime
 import csv
 
-# Database location (Relative path)
-DUMP_DIR = os.expanduser("~/fitbit")
+# Directory in which to dump the csv files
+DUMP_DIR = os.path.expanduser("~/fitbit")
+
+# The list of client methods to use to get data
+# Right now the output csv files will be named after the method
+# and the columns of the csv files will be named the same as the keys
+# in the dictionaries returned by that method
+DATA  = [ 'historical',
+         'sleep_log',
+         'intraday_steps',
+         'intraday_calories_burned',
+         'intraday_active_score',
+         'intraday_sleep',
+         'logged_activities',
+         'activity_records',
+         'intraday_activity_records' ]
 
 # Fitbit Configuration 
 CONFIG = ConfigParser.ConfigParser()
@@ -30,109 +45,39 @@ def client():
                   CONFIG.get('fitbit', 'uis'))
 
 
-
-
-    # # historical
-    # historical = c.historical(date)
-    # session.add_all([ HistoricalData(**x) for x in historical ])
-    
-    # # Sleep Records
-    # sleeprecords = c.sleep_log(date)
-    # session.add_all([ SleepLog(**x) for x in sleeprecords ])
-
-    # # Intraday
-    # # Steps
-    # steps = c.intraday_steps(date)
-    # session.add_all([Steps(**x) for x in steps])
-
-    # # Calories
-    # calories = c.intraday_calories_burned(date)
-    # session.add_all([Calories(**x) for x in calories])
-
-    # # ActiveScore
-    # active_scores = c.intraday_active_score(date)
-    # session.add_all([ActiveScore(**x) for x in active_scores])
-
-    # # Sleep
-    # sleepobs = c.intraday_sleep(date)
-    # session.add_all([Sleep(**x) for x in sleepobs])
-
-    # # Activities
-
-    # # Logged Activities
-    # logged_activities = c.logged_activities(date)
-    # session.add_all([LoggedActivity(**x) for x in logged_activities])
-
-    # # Activity Records
-    # activity_records = c.activity_records(date)
-    # session.add_all([ActivityRecord(**x) for x in activity_records])
-
-    # # Intraday Activity Records
-    # intraday_activity_records = c.intraday_activity_records(date)
-    # session.add_all([IntradayActivityRecord(**x)
-    #                  for x in intraday_activity_records])
-
-    # # Commit all changes
-    # session.flush()
-
-# The list of methods to use to get data
-# Right now the output csv files will be named after the method
-# and the columns of the csv files will be named the same as the keys
-# in the dictionaries returned by that method
-DataFiles = ['historical',
-             'intraday_steps']
-
 def dump_data(method, date):
 
     c = client()
     data = c.__getattribute__(method)(date)
 
-    # name of csv file, right now it is method name + csv
+    # name of csv file, right now it is method name + .csv
     fname = os.path.join(DUMP_DIR, method + '.csv')
     if os.path.isfile(fname):
         exists = True
     else:
         exists = False
-        
-    f = open(fname, 'a')
-    writer = csv.DictWriter(f, data[0].keys())
 
-    # If it is a new file, then write a header row
-    if not exists:
-        header = dict( (x,x) for x in data[0].keys())
-        writer.writerow(header)
+    if len(data) > 0:
+        f = open(fname, 'a')
+        writer = csv.DictWriter(f, data[0].keys())
 
-    # write data to file
-    writer.writerows(data)
-    f.close() 
+        # If it is a new file, then write a header row
+        if not exists:
+            header = dict( (x,x) for x in data[0].keys())
+            writer.writerow(header)
+            
+        # write data to file
+        writer.writerows(data)
+        f.close() 
 
 if __name__ == '__main__':
     
-    # The script takes a single argument for the date you want to update
+    # The script takes a single argument
     if len(sys.argv) < 2:
         sys.exit("Usage: %s YYYY-MM-DD" % sys.argv[0])
-
     
     date = datetime.datetime.strptime(sys.argv[1], "%Y-%m-%d").date()
 
-    c = client()
-    data = c.historical(date)
-
-    fname = os.path.join(DIR_DUMP, 'historical.csv')
-    if os.path.isfile(fname):
-        exists = True
-    else:
-        exists = False
-        
-    f = open(fname, 'a')
-    writer = csv.DictWriter(f, historical[0].keys())
-
-    # If it is a new file, then write a header row
-    if not exists:
-        header = dict( (x,x) for x in historical[0].keys())
-        writer.writerow(header)
-
-    writer.writerows(data)
-    f.close()
-    
+    for method in DATA:
+        dump_data(method, date)
 
